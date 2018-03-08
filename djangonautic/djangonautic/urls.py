892e34b7f7326d24829ee3e5df415513f994c2cc
pearlmiumiu <ll523@scarletmail.rutgers.edu>
2018-03-07 20:47:05 -0500

    Update urls.py

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^about/$', views.about),
-    url(r'^$', views.homepage),
+    url(r'^admin/', admin.site.urls), ### after regular express, it comes with a function
+    url(r'^about/$', views.about), ### $ sign means nothing comes after, it ends with about
+    url(r'^$', views.homepage), ### home page usually has nothing after.  for instance: .com/
 ]
+
+### after we create the view url, we create a new view file.  views.py . inside the views files, is where we create function that will fire 
+### when users visit certain url
