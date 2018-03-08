8dd4074edd481158f87711ecb8268d7403c41134
pearlmiumiu <ll523@scarletmail.rutgers.edu>
2018-03-07 23:15:33 -0500

    Update views.py

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Article
 
 def article_list(request):
-    articles = Article.objects.all().order_by('date');
-    return render(request, 'articles/article_list.html', { 'articles': articles })
+    articles = Article.objects.all().order_by('date');  ### sort articles by date//// send articles to the template so we can output data through template
+    return render(request, 'articles/article_list.html', { 'articles': articles }) ### the data we want to send to template, key name can be changed.
