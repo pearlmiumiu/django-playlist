bb48a50389a161bdfad2dd8836295c3043ef1528
pearlmiumiu <ll523@scarletmail.rutgers.edu>
2018-03-07 20:54:50 -0500

    Update views.py

--- djangonautic/djangonautic/views.py
+++ djangonautic/djangonautic/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse  ### send response to user when they request the page back.
+
+### we need to two functions according to the urls we set up in the urls.py file.
+### we dont need to set function for admin 
+
 
 def homepage(request):
     return HttpResponse('homepage')
 
-def about(request):
+def about(request):  ### response to about url.  request object parameter. 
     return HttpResponse('about')
