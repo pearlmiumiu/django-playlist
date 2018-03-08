bac1a4036949ff622f9761f04ab97fd404dc1c1b
pearlmiumiu <ll523@scarletmail.rutgers.edu>
2018-03-07 21:12:34 -0500

    Update views.py

--- djangonautic/djangonautic/views.py
+++ djangonautic/djangonautic/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render ### render the page, so we need to import render 
 
 def homepage(request):
     # return HttpResponse('homepage')
-    return render(request, 'homepage.html')
+    return render(request, 'homepage.html') ### render( request, page)
 
 def about(request):
     # return HttpResponse('about')
